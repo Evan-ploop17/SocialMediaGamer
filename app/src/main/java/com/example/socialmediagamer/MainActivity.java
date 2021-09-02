dfaa98752be48aff107328f8a43dc33ddf2221f3
Esteban Salas <esalas21@uan.edu.co>
2021-09-01 19:07:59 -0500

    Permite autenticación con google

--- app/src/main/java/com/example/socialmediagamer/MainActivity.java
+++ app/src/main/java/com/example/socialmediagamer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.socialmediagamer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.stream.Stream;
 
@@ -18,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
     TextView mTextViewRegister;
     TextInputEditText mTextinputEmail, mTextinputPassword;
     Button btnLogin;
+    FirebaseAuth auth;
+    // Esto se puede sacar del repo
+    private GoogleSignInClient mGoogleSignInClient;
+    private final int REQUEST_CODE_GOOGLE = 9001;
+    SignInButton btnLoginGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +48,28 @@ public class MainActivity extends AppCompatActivity {
         mTextinputEmail = findViewById(R.id.textInputMail);
         mTextinputPassword = findViewById(R.id.textInputPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnLoginGoogle = findViewById(R.id.btnLoginGoogle);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // inicializamos al instancia
+        auth = FirebaseAuth.getInstance();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 login();
+            }
+        });
+
+        btnLoginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInGoogle();
             }
         });
 
@@ -45,11 +82,66 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // Con este método estará esperando que el usuario selecciona la cuenta de google con l que se registrará
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, REQUEST_CODE_GOOGLE);
+    }
+
+    // Espera hasta que el usuario escoja la cuenta
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == REQUEST_CODE_GOOGLE) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("ERROR", "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("ERROR", "Google sign in failed", e);
+            }
+        }
+    }
+    // Desde acá si ejecuta la autenticación
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            Log.d("ERROR", "signInWithCredential:success");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("ERROR", "signInWithCredential:failure", task.getException());
+                        }
+                    }
+                });
+    }
+    // Método pra entrar solamente con correo y contraseña
     private void login(){
         String mail = mTextinputEmail.getText().toString();
         String pass = mTextinputPassword.getText().toString();
-        Log.d("Prueba", "Mail " + mail);
-        Log.d("Prueba", "Pass " + pass);
+
+        auth.signInWithEmailAndPassword(mail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "X Wrong X", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
 }
